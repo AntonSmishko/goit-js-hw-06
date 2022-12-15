687314025575945ab1b92e4af82b462b4a480bcf
AntonSmishko <anton.smishko2014@gmail.com>
2022-12-15 22:03:41 +0200

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const refIngredients = document.querySelector('#ingredients')
+console.log(refIngredients);
+
+
+const makeNewEl = (ingredient) => { 
+  return ingredients.map(opt => {
+    const newEl = document.createElement('li');
+    newEl.textContent = opt;
+    newEl.classList.add('item')
+    return newEl;
+  })
+}; 
+    
+const elements = makeNewEl(ingredients);
+refIngredients.append(...elements)
+
